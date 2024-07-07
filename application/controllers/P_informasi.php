64d0461d55a23bb5a4f6ce6e2fa13d87ec77d1e9
NitaNuraliza <76465390+Nita29@users.noreply.github.com>
2024-07-07 22:34:43 +0700

    Update P_informasi.php

--- application/controllers/P_informasi.php
+++ application/controllers/P_informasi.php
@@ -66,7 +66,7 @@ class P_informasi extends CI_Controller
         $this->load->library('upload', $config);
         $this->upload->initialize($config);
 
-        if (!$this->upload->do_upload('resume')) {
+        /* if (!$this->upload->do_upload('resume')) {
         } else {
 
             $config['upload_path']          = './permohonan/surat/';
@@ -113,7 +113,7 @@ class P_informasi extends CI_Controller
                     redirect('P_informasi/add');
                 }
             }
-        }
+        } */
 
 
 
@@ -134,124 +134,115 @@ class P_informasi extends CI_Controller
 
 
 
-        // if (!$this->upload->do_upload('surat')) {
-        // } else {
-        //     //tanda pengenal 
-        //     $resume = $this->upload->data();
-        //     $resume = $resume['file_name'];
-        //     $surat = $this->upload->data();
-        //     $surat = $surat['file_name'];
+        if (!$this->upload->do_upload('surat')) {
+        } else {
+            //tanda pengenal 
+            $resume = $this->upload->data();
+            $resume = $resume['file_name'];
+            $surat = $this->upload->data();
+            $surat = $surat['file_name'];
 
-        //     $tanggal_masuk = $this->input->post('tanggal_masuk', TRUE);
-        //     $jenis = $this->input->post('jenis', TRUE);
-        //     $nama = $this->input->post('nama', TRUE);
-        //     $alamat = $this->input->post('alamat');
-        //     $jenis_kelamin = $this->input->post('jenis_kelamin', TRUE);
-        //     $kecamatan = $this->input->post('kecamatan', TRUE);
-        //     $kelurahan = $this->input->post('kelurahan', TRUE);
-        //     $kab = $this->input->post('kab', TRUE);
-        //     $provinsi = $this->input->post('provinsi', TRUE);
-        //     $kodepos = $this->input->post('kodepos', TRUE);
-        //     $email = $this->input->post('email', TRUE);
-        //     $mobile = $this->input->post('mobile', TRUE);
-        //     $nik = $this->input->post('nik', TRUE);
-        //     $tema = $this->input->post('tema', TRUE);
-        //     $tujuan = $this->input->post('tujuan', TRUE);
-
-
-        //     $data = array(
-        //         'tanggal_masuk' => $tanggal_masuk,
-        //         'jenis' => $jenis,
-        //         'nama' => $nama,
-        //         'alamat' => $alamat,
-        //         'jenis_kelamin' => $jenis_kelamin,
-        //         'kecamatan' => $kecamatan,
-        //         'kelurahan' => $kelurahan,
-        //         'kab' => $kab,
-        //         'provinsi' => $provinsi,
-        //         'kodepos' => $kodepos,
-        //         'email' => $email,
-        //         'mobile' => $mobile,
-        //         'nik' => $nik,
-        //         'resume' => $resume,
-        //         'surat' => $surat,
-        //         'tema' => $tema,
-        //         'tujuan' => $tujuan,
-        //     );
-        //     $save = $this->db->insert('permohonan', $data);
-        //     if ($save) {
-        //         set_pesan('Permohonan diproses , Harap tunggu , dan cek email beberapa waktu .');
-        //         redirect('p_informasi');
-        //     } else {
-        //         set_pesan('Permohonan gagal ,silahkan cek kembali', false);
-        //         redirect('p_informasi/add');
-        //     }
-        // }
-
-        // if ($this->form_validation->run() == false) {
-
-        //     $resume = $this->upload->data();
-        //     $resume = $resume['file_name'];
-        //     $surat = $this->upload->data();
-        //     $surat = $surat['file_name'];
-
-        //     $tanggal_masuk = $this->input->post('tanggal_masuk');
-        //     $jenis = $this->input->post('jenis', TRUE);
-        //     $nama = $this->input->post('nama', TRUE);
-        //     $alamat = $this->input->post('alamat');
-        //     $jenis_kelamin = $this->input->post('jenis_kelamin', TRUE);
-        //     $kecamatan = $this->input->post('kecamatan', TRUE);
-        //     $kelurahan = $this->input->post('kelurahan', TRUE);
-        //     $kab = $this->input->post('kab', TRUE);
-        //     $provinsi = $this->input->post('provinsi', TRUE);
-        //     $kodepos = $this->input->post('kodepos', TRUE);
-        //     $email = $this->input->post('email', TRUE);
-        //     $mobile = $this->input->post('mobile', TRUE);
-        //     $nik = $this->input->post('nik', TRUE);
-        //     $tema = $this->input->post('tema', TRUE);
-        //     $tujuan = $this->input->post('tujuan', TRUE);
+            $tanggal_masuk = $this->input->post('tanggal_masuk', TRUE);
+            $jenis = $this->input->post('jenis', TRUE);
+            $nama = $this->input->post('nama', TRUE);
+            $alamat = $this->input->post('alamat');
+            $jenis_kelamin = $this->input->post('jenis_kelamin', TRUE);
+            $kecamatan = $this->input->post('kecamatan', TRUE);
+            $kelurahan = $this->input->post('kelurahan', TRUE);
+            $kab = $this->input->post('kab', TRUE);
+            $provinsi = $this->input->post('provinsi', TRUE);
+            $kodepos = $this->input->post('kodepos', TRUE);
+            $email = $this->input->post('email', TRUE);
+            $mobile = $this->input->post('mobile', TRUE);
+            $nik = $this->input->post('nik', TRUE);
+            $tema = $this->input->post('tema', TRUE);
+            $tujuan = $this->input->post('tujuan', TRUE);
 
 
-        //     $data = array(
-        //         'tanggal_masuk' => $tanggal_masuk,
-        //         'jenis' => $jenis,
-        //         'nama' => $nama,
-        //         'alamat' => $alamat,
-        //         'jenis_kelamin' => $jenis_kelamin,
-        //         'kecamatan' => $kecamatan,
-        //         'kelurahan' => $kelurahan,
-        //         'kab' => $kab,
-        //         'provinsi' => $provinsi,
-        //         'kodepos' => $kodepos,
-        //         'email' => $email,
-        //         'mobile' => $mobile,
-        //         'nik' => $nik,
-        //         'resume' => $resume,
-        //         'surat' => $surat,
-        //         'tema' => $tema,
-        //         'tujuan' => $tujuan,
-        //     );
-        //     $save = $this->db->insert('permohonan', $data);
-        // } else {
-        //     $input = $this->input->post(null, true);
-        //     $save = $this->db->insert('permohonan', $input);
-        //     if ($save) {
-        //         set_pesan('Permohonan diproses , Harap tunggu , dan cek email beberapa waktu.');
-        //         redirect('permohonan');
-        //     } else {
-        //         set_pesan('Permohonan gagal ,silahkan cek kembali', false);
-        //         redirect('permohonan/add');
-        //     }
-        // }
+            $data = array(
+                'tanggal_masuk' => $tanggal_masuk,
+                'jenis' => $jenis,
+                'nama' => $nama,
+                'alamat' => $alamat,
+                'jenis_kelamin' => $jenis_kelamin,
+                'kecamatan' => $kecamatan,
+                'kelurahan' => $kelurahan,
+                'kab' => $kab,
+                'provinsi' => $provinsi,
+                'kodepos' => $kodepos,
+                'email' => $email,
+                'mobile' => $mobile,
+                'nik' => $nik,
+                'resume' => $resume,
+                'surat' => $surat,
+                'tema' => $tema,
+                'tujuan' => $tujuan,
+            );
+            $save = $this->db->insert('permohonan', $data);
+            if ($save) {
+                set_pesan('Permohonan diproses , Harap tunggu , dan cek email beberapa waktu .');
+                redirect('p_informasi');
+            } else {
+                set_pesan('Permohonan gagal ,silahkan cek kembali', false);
+                redirect('p_informasi/add');
+            }
+        }
+
+        if ($this->form_validation->run() == false) {
+
+            $resume = $this->upload->data();
+            $resume = $resume['file_name'];
+            $surat = $this->upload->data();
+            $surat = $surat['file_name'];
+
+            $tanggal_masuk = $this->input->post('tanggal_masuk');
+            $jenis = $this->input->post('jenis', TRUE);
+            $nama = $this->input->post('nama', TRUE);
+            $alamat = $this->input->post('alamat');
+            $jenis_kelamin = $this->input->post('jenis_kelamin', TRUE);
+            $kecamatan = $this->input->post('kecamatan', TRUE);
+            $kelurahan = $this->input->post('kelurahan', TRUE);
+            $kab = $this->input->post('kab', TRUE);
+            $provinsi = $this->input->post('provinsi', TRUE);
+            $kodepos = $this->input->post('kodepos', TRUE);
+            $email = $this->input->post('email', TRUE);
+            $mobile = $this->input->post('mobile', TRUE);
+            $nik = $this->input->post('nik', TRUE);
+            $tema = $this->input->post('tema', TRUE);
+            $tujuan = $this->input->post('tujuan', TRUE);
 
 
-
-
-
-
-
-
-        // ------------------------------------------------------------------
+            $data = array(
+                'tanggal_masuk' => $tanggal_masuk,
+                'jenis' => $jenis,
+                'nama' => $nama,
+                'alamat' => $alamat,
+                'jenis_kelamin' => $jenis_kelamin,
+                'kecamatan' => $kecamatan,
+                'kelurahan' => $kelurahan,
+                'kab' => $kab,
+                'provinsi' => $provinsi,
+                'kodepos' => $kodepos,
+                'email' => $email,
+                'mobile' => $mobile,
+                'nik' => $nik,
+                'resume' => $resume,
+                'surat' => $surat,
+                'tema' => $tema,
+                'tujuan' => $tujuan,
+            );
+            $save = $this->db->insert('permohonan', $data);
+        } else {
+            $input = $this->input->post(null, true);
+            $save = $this->db->insert('permohonan', $input);
+            if ($save) {
+                set_pesan('Permohonan diproses , Harap tunggu , dan cek email beberapa waktu.');
+                redirect('permohonan');
+            } else {
+                set_pesan('Permohonan gagal ,silahkan cek kembali', false);
+                redirect('permohonan/add');
+            }
+        }
 
 
 
@@ -260,55 +251,7 @@ class P_informasi extends CI_Controller
 
 
 
-
-        // if (!$this->upload->do_upload('resume')) {
-        //     echo "gagal";
-        // } else {
-        //     $resume = $this->upload->data();
-        //     $resume = $resume['file_name'];
-
-        //     $jenis = $this->input->post('jenis', TRUE);
-        //     $nama = $this->input->post('nama', TRUE);
-        //     $alamat = $this->input->post('alamat', TRUE);
-        //     $jenis_kelamin = $this->input->post('jenis_kelamin', TRUE);
-        //     $kecamatan = $this->input->post('kecamatan', TRUE);
-        //     $kelurahan = $this->input->post('kelurahan', TRUE);
-        //     $kab = $this->input->post('kab', TRUE);
-        //     $provinsi = $this->input->post('provinsi', TRUE);
-        //     $kodepos = $this->input->post('kodepos', TRUE);
-        //     $email = $this->input->post('email', TRUE);
-        //     $mobile = $this->input->post('mobile', TRUE);
-        //     $nik = $this->input->post('nik', TRUE);
-        //     $tema = $this->input->post('tema', TRUE);
-        //     $tujuan = $this->input->post('tujuan', TRUE);
-
-
-        //     $data = array(
-        //         'jenis' => $jenis,
-        //         'nama' => $nama,
-        //         'alamat' => $alamat,
-        //         'jenis_kelamin' => $jenis_kelamin,
-        //         'kecamatan' => $kecamatan,
-        //         'kelurahan' => $kelurahan,
-        //         'kab' => $kab,
-        //         'provinsi' => $provinsi,
-        //         'kodepos' => $kodepos,
-        //         'email' => $email,
-        //         'mobile' => $mobile,
-        //         'nik' => $nik,
-        //         'resume' => $resume,
-        //         'tema' => $tema,
-        //         'tujuan' => $tujuan,
-        //     );
-        //     $this->db->insert('permohonan', $data);
-        // }
-
-
-
-
-
-
-        // --------------------------------------------------------------------
+        //------------------------------------------------------------------
 
 
 
@@ -318,57 +261,114 @@ class P_informasi extends CI_Controller
 
 
 
+        if (!$this->upload->do_upload('resume')) {
+            echo "gagal";
+        } else {
+            $resume = $this->upload->data();
+            $resume = $resume['file_name'];
 
-        // if ($this->form_validation->run() == false) {
+            $jenis = $this->input->post('jenis', TRUE);
+            $nama = $this->input->post('nama', TRUE);
+            $alamat = $this->input->post('alamat', TRUE);
+            $jenis_kelamin = $this->input->post('jenis_kelamin', TRUE);
+            $kecamatan = $this->input->post('kecamatan', TRUE);
+            $kelurahan = $this->input->post('kelurahan', TRUE);
+            $kab = $this->input->post('kab', TRUE);
+            $provinsi = $this->input->post('provinsi', TRUE);
+            $kodepos = $this->input->post('kodepos', TRUE);
+            $email = $this->input->post('email', TRUE);
+            $mobile = $this->input->post('mobile', TRUE);
+            $nik = $this->input->post('nik', TRUE);
+            $tema = $this->input->post('tema', TRUE);
+            $tujuan = $this->input->post('tujuan', TRUE);
 
 
-        //     $jenis = $this->input->post('jenis', TRUE);
-        //     $nama = $this->input->post('nama', TRUE);
-        //     $alamat = $this->input->post('alamat');
-        //     $jenis_kelamin = $this->input->post('jenis_kelamin', TRUE);
-        //     $kecamatan = $this->input->post('kecamatan', TRUE);
-        //     $kelurahan = $this->input->post('kelurahan', TRUE);
-        //     $kab = $this->input->post('kab', TRUE);
-        //     $provinsi = $this->input->post('provinsi', TRUE);
-        //     $kodepos = $this->input->post('kodepos', TRUE);
-        //     $email = $this->input->post('email', TRUE);
-        //     $mobile = $this->input->post('mobile', TRUE);
-        //     $nik = $this->input->post('nik', TRUE);
-        //     // $resume = $this->input->post('resume', TRUE);
-        //     $tema = $this->input->post('tema', TRUE);
-        //     $tujuan = $this->input->post('tujuan', TRUE);
+            $data = array(
+                'jenis' => $jenis,
+                'nama' => $nama,
+                'alamat' => $alamat,
+                'jenis_kelamin' => $jenis_kelamin,
+                'kecamatan' => $kecamatan,
+                'kelurahan' => $kelurahan,
+                'kab' => $kab,
+                'provinsi' => $provinsi,
+                'kodepos' => $kodepos,
+                'email' => $email,
+                'mobile' => $mobile,
+                'nik' => $nik,
+                'resume' => $resume,
+                'tema' => $tema,
+                'tujuan' => $tujuan,
+            );
+            $this->db->insert('permohonan', $data);
+        }
 
-        //     $data = array(
-        //         'jenis' => $jenis,
-        //         'nama' => $nama,
-        //         'alamat' => $alamat,
-        //         'jenis_kelamin' => $jenis_kelamin,
-        //         'kecamatan' => $kecamatan,
-        //         'kelurahan' => $kelurahan,
-        //         'kab' => $kab,
-        //         'provinsi' => $provinsi,
-        //         'kodepos' => $kodepos,
-        //         'email' => $email,
-        //         'mobile' => $mobile,
-        //         'nik' => $nik,
-        //         // 'resume' => $resume,
-        //         'tema' => $tema,
-        //         'tujuan' => $tujuan,
 
-        //     );
-        //     $this->admin->insert('permohonan', $data);
 
-        //     $this->load->view("permohonan", $data);
-        // } else {
-        //     $input = $this->input->post(null, true);
-        //     $save = $this->admin->insert('permohonan', $input);
-        //     if ($save) {
-        //         set_pesan('data berhasil disimpan.');
-        //         redirect('permohonan');
-        //     } else {
-        //         set_pesan('data gagal disimpan', false);
-        //         redirect('permohonan/add');
-        //     }
-        // }
+
+
+
+        //--------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+        if ($this->form_validation->run() == false) {
+
+
+            $jenis = $this->input->post('jenis', TRUE);
+            $nama = $this->input->post('nama', TRUE);
+            $alamat = $this->input->post('alamat');
+            $jenis_kelamin = $this->input->post('jenis_kelamin', TRUE);
+            $kecamatan = $this->input->post('kecamatan', TRUE);
+            $kelurahan = $this->input->post('kelurahan', TRUE);
+            $kab = $this->input->post('kab', TRUE);
+            $provinsi = $this->input->post('provinsi', TRUE);
+            $kodepos = $this->input->post('kodepos', TRUE);
+            $email = $this->input->post('email', TRUE);
+            $mobile = $this->input->post('mobile', TRUE);
+            $nik = $this->input->post('nik', TRUE);
+            // $resume = $this->input->post('resume', TRUE);
+            $tema = $this->input->post('tema', TRUE);
+            $tujuan = $this->input->post('tujuan', TRUE);
+
+            $data = array(
+                'jenis' => $jenis,
+                'nama' => $nama,
+                'alamat' => $alamat,
+                'jenis_kelamin' => $jenis_kelamin,
+                'kecamatan' => $kecamatan,
+                'kelurahan' => $kelurahan,
+                'kab' => $kab,
+                'provinsi' => $provinsi,
+                'kodepos' => $kodepos,
+                'email' => $email,
+                'mobile' => $mobile,
+                'nik' => $nik,
+                // 'resume' => $resume,
+                'tema' => $tema,
+                'tujuan' => $tujuan,
+
+            );
+            $this->admin->insert('permohonan', $data);
+
+            $this->load->view("permohonan", $data);
+        } else {
+            $input = $this->input->post(null, true);
+            $save = $this->admin->insert('permohonan', $input);
+            if ($save) {
+                set_pesan('data berhasil disimpan.');
+                redirect('permohonan');
+            } else {
+                set_pesan('data gagal disimpan', false);
+                redirect('permohonan/add');
+            }
+        }
     }
 }
